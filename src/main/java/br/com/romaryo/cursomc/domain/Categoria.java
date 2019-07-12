c11386e3b518b5402f03546db39bde5fb6f5dd79
Romaryo Ricardo <romaryoricardo@gmail.com>
2019-07-12 09:58:17 -0300

    Criando respository e service para Categoria

--- src/main/java/br/com/romaryo/cursomc/domain/Categoria.java
+++ src/main/java/br/com/romaryo/cursomc/domain/Categoria.java
@@ -1,4 +1,4 @@
-package br.com.romaryo.cursomc.domain;
+ package br.com.romaryo.cursomc.domain;
 
 import java.io.Serializable;
 
--- src/main/java/br/com/romaryo/cursomc/resourses/CategoriaResource.java
+++ src/main/java/br/com/romaryo/cursomc/resourses/CategoriaResource.java
@@ -1,30 +1,28 @@
 package br.com.romaryo.cursomc.resourses;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.romaryo.cursomc.domain.Categoria;
+import br.com.romaryo.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
+	@Autowired
+	private CategoriaService service;
 	
-	public List<Categoria> listar() {
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria cat1 = new Categoria(1,"Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+		Categoria obj = service.buscar(id);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 }
