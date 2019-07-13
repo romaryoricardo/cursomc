a1703d30127b8be5dcb4fad9d9178177f56600d9
Romaryo Ricardo <romaryoricardo@gmail.com>
2019-07-13 19:59:53 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/romaryo/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.romaryo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.romaryo.cursomc.domain.Cliente;
+import br.com.romaryo.cursomc.repositories.ClienteRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) throws ObjectNotFoundException{
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id:" + id + "  Tipo: " + Cliente.class.getName()));
+	}
+}
